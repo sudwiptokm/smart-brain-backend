2f1d991d2408b3f7a12e21c174622c38be0e7b12
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-11-29 04:52:01 +0600

    a

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express")
 const bcrypt = require("bcrypt-nodejs")
+const cors = require("cors")
 
 const app = express()
+
 app.use(express.json())
+app.use(cors())
 
 const database = {
     users : [
         {
             id: "123",
             name: "John",
-            email: "john@gmail.com",
-            password: "cookies",
+            email: "1",
+            password: "1",
             entries: 0,
             joined: new Date()
         },
@@ -23,13 +26,13 @@ const database = {
             joined: new Date()
         }
     ],
-    login: [
-        {
-            id: "987",
-            email: "john@gmail.com",
-            hash : ""
-        }
-    ]
+    // login: [
+    //     {
+    //         id: "987",
+    //         email: "john@gmail.com",
+    //         hash : ""
+    //     }
+    // ]
 }
 
 app.get("/", (req,res) => {
@@ -38,15 +41,15 @@ app.get("/", (req,res) => {
 
 app.post("/signin", (req,res) => {
     // Load hash from your password DB.
-    bcrypt.compare("apples", "$2a$10$/.0cer/WAEGzLNV/gVLHluFeo1B4aJ2zX8Pr4Xjp/R81/KDEOfLjK", function(err, res) {
-        console.log("1st guess", res)
-    });
-    bcrypt.compare("veggies", "$2a$10$/.0cer/WAEGzLNV/gVLHluFeo1B4aJ2zX8Pr4Xjp/R81/KDEOfLjK", function(err, res) {
-        console.log("2nd guess", res)
-    });
+    // bcrypt.compare("apples", "$2a$10$/.0cer/WAEGzLNV/gVLHluFeo1B4aJ2zX8Pr4Xjp/R81/KDEOfLjK", function(err, res) {
+    //     console.log("1st guess", res)
+    // });
+    // bcrypt.compare("veggies", "$2a$10$/.0cer/WAEGzLNV/gVLHluFeo1B4aJ2zX8Pr4Xjp/R81/KDEOfLjK", function(err, res) {
+    //     console.log("2nd guess", res)
+    // });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
-            res.json("Successful")
+            res.json(database.users[0])
     }
     else{
         res.status(400).json("Error Logging In")
@@ -55,17 +58,16 @@ app.post("/signin", (req,res) => {
 
 app.post("/register", (req,res) => {
     const {email, name, password} = req.body
-    bcrypt.hash(password, null, null, function(err, hash) {
-        // Store hash in your password DB.
-        console.log(hash)
-    });
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     console.log(hash)
+    // });
 
     database.users.push(
         {
             id: "125",
             name: name,
             email: email,
-            password: password,
             entries: 0,
             joined: new Date()
         }
@@ -77,9 +79,9 @@ app.get("/profile/:id", (req,res) => {
     const {id} = req.params;
     let found = false
     database.users.forEach(x => {
-        if (x.id === id){
+        if (user.id === id){
             found = true
-            return res.json(x)
+            return res.json(user)
         }
     })
     if (!found) res.status(404).json("Not Found, Daaaaaaaamn!")
@@ -88,14 +90,14 @@ app.get("/profile/:id", (req,res) => {
 app.put("/image", (req,res) => {
     const {id} = req.body;
     let found = false
-    database.users.forEach(x => {
-        if (x.id === id){
+    database.users.forEach(user => {
+        if (user.id === id){
             found = true
-            x.entries++
-            return res.json(x.entries)
+            user.entries++
+            return res.json(user.entries)
         }
     })
-    if (!found) res.status(404).json("Not Found, Daaaaaaaamn!")
+    if (!found) res.status(400).json("Not Found")
 })
 
 
