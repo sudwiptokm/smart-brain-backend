704efdf5de55a917f36fc8c9cf4b08e014336631
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-11-30 01:36:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require("express")
 const bcrypt = require("bcrypt-nodejs")
 const cors = require("cors")
+const knex = require('knex')
 
 const app = express()
 
-const knex = require('knex')({
+const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      port : 3306,
+      port : 5432,
       user : 'postgres',
       password : '1',
       database : 'smart-brain'
     }
 });
+
+// db.select('*').from('users').then(data => {
+//     console.log(data)
+// })
 
 
 app.use(express.json())
@@ -75,15 +80,11 @@ app.post("/register", (req,res) => {
     //     console.log(hash)
     // });
 
-    database.users.push(
-        {
-            id: "125",
-            name: name,
-            email: email,
-            entries: 0,
-            joined: new Date()
-        }
-    )
+    db('users').insert({
+        email: email,
+        name: name,
+        joined: new Date()
+    }).then(console.log)
     res.json(database.users[database.users.length-1])
 })
 
