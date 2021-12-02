18ae581f2c6074ea74ed9d76876776bd048a7d09
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-12-03 05:56:34 +0600

    API Switch to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require("clarifai")
+
+const app =new Clarifai.App({
+    apiKey: "0f0c3e5c71af4fa2b8c22bc5a2806217"
+})
+
+handleApiCall  = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json("unable to work with API"))
+}
+
+
 const handleImage = (req, res, db) => {
     const {id} = req.body;
     db('users').where('id','=',id)
@@ -8,5 +24,5 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage, handleApiCall
 }
--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.post("/register", (req, res) => {register.handleRegister(req, res, db, bcryp
 app.get("/profile/:id", (req, res) => {profile.handleProfile(req, res, db)})
 
 app.put("/image", (req, res) => {image.handleImage(req, res, db)})
+app.post("/imageurl", (req, res) => {image.handleApiCall(req, res)})
 
 
 app.listen(3000, ()=>{
